9bdcc98dd8e78b392de4025a8e0e8e669e60e75c
Fbarboza2024 <161403879+Fbarboza2024@users.noreply.github.com>
2026-01-18 17:57:05 -0300

    Create browser.js

---
+++ app/core/browser.js
@@ -0,0 +1,17 @@
+import { chromium } from "playwright";
+import fs from "fs";
+
+export async function createContext(cookiesPath) {
+  const browser = await chromium.launch({
+    headless: true
+  });
+
+  const context = await browser.newContext();
+
+  if (cookiesPath && fs.existsSync(cookiesPath)) {
+    const cookies = JSON.parse(fs.readFileSync(cookiesPath));
+    await context.addCookies(cookies);
+  }
+
+  return { browser, context };
+}
