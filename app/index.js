e99b7df2aad19365575e9065a5efce53dbea9613
Fbarboza2024 <161403879+Fbarboza2024@users.noreply.github.com>
2026-01-18 18:57:51 -0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,4 +3,7 @@ import { run } from "./workers/runner.js";
 
 console.log("🚀 FBIM TECH — Social Automation Online");
 
-run();
+run().catch(err => {
+  console.error("❌ Fatal error in FBIM:", err);
+  process.exit(1);
+});
